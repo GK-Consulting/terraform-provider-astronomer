3e21e68ac140dc534650cb9384c7c5db1eb23aea
ikegentz <isaac.gentz@gmail.com>
2023-12-28 14:02:38 -0700

    API wrapper around listing and getting organizations

---
+++ internal/api/org.go
@@ -0,0 +1,70 @@
+package api
+
+import (
+	"fmt"
+	"net/http"
+)
+
+type OrgListResponse struct {
+	Organizations []OrgResponse `json:"organizations"`
+	Limit         int           `json:"limit"`
+	Offset        int           `json:"offset"`
+	TotalCount    int           `json:"totalCount"`
+}
+
+type OrgResponse struct {
+	BillingEmail   string              `json:"billingEmail"`
+	CreatedAt      string              `json:"createdAt"`
+	CreatedBy      BasicSubjectProfile `json:"createdBy"`
+	ID             string              `json:"id"`
+	IsScumEnabled  bool                `json:"isScimEnabled"`
+	ManagedDomains []ManagedDomain     `json:"managedDomains"`
+	Name           string              `json:"name"`
+	PaymentMethod  string              `json:"paymentMethod"`
+	Product        string              `json:"product"`
+	Status         string              `json:"status"`
+	SupportPlan    string              `json:"supportPlan"`
+	TrialExpiresAt string              `json:"trialExpiresAt"`
+	UpdatedAt      string              `json:"updatedAt"`
+	UpdatedBy      BasicSubjectProfile `json:"updatedBy"`
+}
+
+type BasicSubjectProfile struct {
+	APITokenName string `json:"apiTokenName"`
+	AvatarUrl    string `json:"avatarUrl"`
+	FullName     string `json:"fullName"`
+	ID           string `json:"id"`
+	SubjectType  string `json:"subjectType"`
+	Username     string `json:"Username"`
+}
+
+type ManagedDomain struct {
+	CreatedAt      string   `json:"createdAt"`
+	EnforcedLogins []string `json:"enforcedLogins"`
+	ID             string   `json:"id"`
+	Name           string   `json:"name"`
+	Status         string   `json:"status"`
+	UpdatedAt      string   `json:"updatedAt"`
+}
+
+func GetOrgs(apiKey string) (*OrgListResponse, error) {
+	request, _ := http.NewRequest("GET", urlBase, nil)
+	decoded := new(OrgListResponse)
+	err := getObjectFromApi(apiKey, request, &decoded)
+	if err != nil {
+		return nil, fmt.Errorf("%s", err)
+	}
+
+	return decoded, nil
+}
+
+func GetOrg(apiKey string, orgId string) (*OrgResponse, error) {
+	request, _ := http.NewRequest("GET", urlBase+orgId, nil)
+	decoded := new(OrgResponse)
+	err := getObjectFromApi(apiKey, request, &decoded)
+	if err != nil {
+		return nil, fmt.Errorf("%s", err)
+	}
+
+	return decoded, nil
+}
